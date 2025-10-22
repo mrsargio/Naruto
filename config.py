ca1a9e7a846c317d442e840034419415510b60b6
mrsargio <mrsargio5@gmail.com>
2025-10-22 10:05:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,22 @@
 import os
 
 class Config(object):
-    
-    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
-    
-    API_ID = 12475131
-    
-    API_HASH = "719171e38be5a1f500613837b79c536f"
-    ADMIN_ID = [12475131]
-    
+    BOT_TOKEN = "8408491525:AAHWmJv-UJ49QUHC2rZmnqb9vCo_2zSu49E"
+    API_ID = 24250238
+    API_HASH = "cb3f118ce5553dc140127647edcf3720"
+    ADMIN = '6175650047,59194'.split(',')
+    ADMIN_ID = [int(id) for id in ADMIN]
+
+    # 👇 FIX THIS LINE (must end with .net/DBNAME)
     DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    
     DB_NAME = "MY_BOT_DB"
-    
-    TXT_LOG = -1002542634912
-    
-    AUTH_LOG = -1002542634912
-    
-    HIT_LOG = -1002542634912
-    
-    DRM_DUMP = -1002542634912
-    # Main channel ki ID
-    CHANNEL = -1002542634912
-    # Channel ka link
-    CH_URL = "https://t.me/gjskisb"
-    
-    OWNER = "https://t.me/gjskisb"
-    
-    THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
-   
+
+    TXT_LOG = -1003164620330
+    AUTH_LOG = -1003164620330
+    HIT_LOG = -1003164620330
+    DRM_DUMP = -1003164620330
+    CHANNEL = -1003164620330
+    CH_URL = "https://t.me/sujuki27"
+    OWNER = "https://t.me/sujuki27"
+    THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg"
     HOST = "https://api.masterapi.tech"
